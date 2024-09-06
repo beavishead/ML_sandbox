6afadbb2e92bed976c9d998a729637f81d4aa5e8
beavisheader <nosachev019940@gmail.com>
2024-09-06 18:54:02 +0300

    gradient descent

---
+++ restoring_coefficients_184.py
@@ -0,0 +1,43 @@
+"""
+THE FIND-COEF TASK ACCOMPLISHED BY USING THE GRADIENT DESCENT
+"""
+
+import sys
+import numpy as np
+
+# Reading data
+inp = list(map(lambda x: x.split(), sys.stdin.readlines()))
+x, y = zip(*inp)
+x = np.array(list(map(float, x)))
+y = np.array(list(map(float, y)))
+n = float(len(inp))
+
+# Initialize a, b, c
+a, b, c = [np.random.ranf()]*3
+
+# Initialize hyperparameters
+alpha = 0.0001  # Smaller learning rate to stabilize training
+
+for i in range(1000):
+    # Defining function
+    y_hat = (a * np.sin(x) + b * np.log(x))**2 + c * np.power(x, 2)
+G
+    # Computing the errors
+    errors = y-y_hat
+
+    # Counting the derivatives with respect to the cost function
+    da = np.mean(4 * errors * (a * np.sin(x) + b * np.log(x)) * np.sin(x))
+    db = np.mean(4 * errors * (a * np.sin(x) + b * np.log(x)) * np.log(x))
+    dc = np.mean(2 * errors * np.power(x, 2))
+
+    # Update rule
+    a += alpha * da
+    b += alpha * db
+    c += alpha * dc
+
+    # Printing the cost
+    cost = np.mean(np.sqrt(np.power(errors, 2)))
+    print(da,db,dc)
+    print("Cost after iter", i, ":", cost)
+
+print('%.2f'%a,'%.2f' % b,'%.2f' % c)
